236b24fc7d484778472854aa55799d2890dd0d31
xdariel <dariel87@gmail.com>
2020-05-12 14:08:47 -0400

    Change BrowserRouter By HasRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import HomePage from './pages/home-page/home-page.page';
 import CourseIntro2Page from './pages/course-intro-2/course-intro-2.page';
 import CourseIntroPage from './pages/course-intro/course-intro.page';
@@ -7,12 +7,13 @@ import CourseIntroPage from './pages/course-intro/course-intro.page';
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/course-intro' component={CourseIntroPage} />
-      <Route path='/course-intro-2' component={CourseIntro2Page} />
-    </Switch>
-  </div>
+      <HashRouter>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/course-intro' component={CourseIntroPage} />
+        <Route path='/course-intro-2' component={CourseIntro2Page} />
+      </HashRouter>
+
+    </div>
   );
 }
 
