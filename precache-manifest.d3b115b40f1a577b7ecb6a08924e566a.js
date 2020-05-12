4a04c7a95f005f523cc62b6a3c1148bea51ca18a
xdariel <dariel87@gmail.com>
2020-05-12 09:01:19 -0400

    Updates

---
+++ precache-manifest.d3b115b40f1a577b7ecb6a08924e566a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24686d7feba3659cf574be1844e09d1f",
+    "url": "/courseplus/index.html"
+  },
+  {
+    "revision": "a53598c4bc9f9c1214ed",
+    "url": "/courseplus/static/js/2.3a9e79e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/courseplus/static/js/2.3a9e79e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c0fd506b771a02c826",
+    "url": "/courseplus/static/js/main.9e8b5355.chunk.js"
+  },
+  {
+    "revision": "1b70e275c9556bada7d4",
+    "url": "/courseplus/static/js/runtime-main.4ed02981.js"
+  }
+]);
