02d3fce759ad011dc25e52f266d906386206f2b2
xdariel <dariel87@gmail.com>
2020-05-13 12:57:18 -0400

    Updates

---
+++ precache-manifest.0d4073e9d158c09511033dd8dcd4a676.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d294268c8de8962e42b59bd759a8a87e",
+    "url": "/courseplus/index.html"
+  },
+  {
+    "revision": "e7c495600786936d1d6a",
+    "url": "/courseplus/static/js/2.0052682c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/courseplus/static/js/2.0052682c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5fd01df72eabfa744d",
+    "url": "/courseplus/static/js/main.2a32f056.chunk.js"
+  },
+  {
+    "revision": "1b70e275c9556bada7d4",
+    "url": "/courseplus/static/js/runtime-main.4ed02981.js"
+  }
+]);
